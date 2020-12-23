15c20708918490390288b6b4f49f886a451c67e5
huwd <hwd9053@qq.com>
2020-12-23 23:48:42 +0800

    20201223 night

--- 12-堆/src/com/mj/heap/BinaryHeap.java
+++ 12-堆/src/com/mj/heap/BinaryHeap.java
@@ -3,31 +3,19 @@ package com.mj.heap;
 import java.util.Comparator;
 
 @SuppressWarnings("unchecked")
-public class BinaryHeap<E> implements Heap<E> {
+public class BinaryHeap<E> extends AbstractHeap<E> {
 
     private E[] elements;
-    private int size;
-    private Comparator<E> comparator; // 二叉堆要求元素具有可比较性
 
     private static final int DEFAULT_CAPACITY = 10;
 
     public BinaryHeap(Comparator<E> comparator) {
-        this.comparator = comparator;
-        elements = (E[]) new Object[DEFAULT_CAPACITY];
+        super(comparator);
+        this.elements = (E[]) new Object[DEFAULT_CAPACITY];
     }
 
     public BinaryHeap() {
         this(null);
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
     }
 
     @Override
@@ -55,25 +43,95 @@ public class BinaryHeap<E> implements Heap<E> {
 
     @Override
     public E remove() {
-        return null;
+        emptyCheck();
+        E root = elements[0];
+        int lastIndex = --size;
+        elements[0] = elements[lastIndex];
+        elements[lastIndex] = null;
+        siftDown(0);
+
+        return root;
     }
 
     @Override
     public E replace(E element) {
-        return null;
+        elementNotNullCheck(element);
+        E root = null;
+        if (size == 0) {
+            elements[0] = element;
+            size++;
+        } else {
+            root = elements[0];
+            elements[0] = element;
+            siftDown(0);
+        }
+        return root;
     }
 
     // 让index位置的元素上滤
     private void siftUp(int index) {
+        // 3*O(logn)
+//        E e = elements[index];
+//        while (index > 0) {
+//            int pIndex = (index - 1) >> 1;
+//            E p = elements[pIndex];
+//            if (compare(e, p) <= 0) return;
+//
+//            // 交换index，pIndex位置的内容
+//            E tmp = elements[index];
+//            elements[index] = elements[pIndex];
+//            elements[pIndex] = tmp;
+//            // 重新赋值index
+//            index = pIndex;
+//        }
+
+        // O(logn) + 1
+        E element = elements[index];
         while (index > 0) {
+            int parentIndex = (index - 1) >> 1;
+            E parent = elements[parentIndex];
+            if (compare(element, parent) <= 0) break;
+
+            // 将父元素存储在index位置
+            elements[index] = elements[parentIndex];
+            // 重新赋值index
+            index = parentIndex;
+        }
+        elements[index] = element;
+    }
+
+    // 下滤
+    private void siftDown(int index) {
+        E element = elements[index];
+        int half = size >> 1;
+        // 第一个叶子节点的索引 == 非叶子节点的数量
+        while (index < half) { // 必须保证index位置的节点是非叶子节点(叶子节点无需也无法下滤)
+            // index的节点有两种情况
+            // 1.只有左子节点
+            // 2.同时有左右子节点
+
+            // 默认为左子节点,跟左子节点进行比较
+            int childIndex = (index << 1) + 1;
+            E child = elements[childIndex];
+
+            // 右子节点
+            int rightIndex = childIndex + 1;
+            // 选出左右子节点中最大的那个
+            if (rightIndex < size && compare(elements[rightIndex], child) > 0) {
+                child = elements[childIndex = rightIndex];
+            }
+
+            if (compare(element, child) >= 0) break;
+
+            // 将子节点存放到index位置
+            elements[index] = child;
+            index = childIndex;
 
         }
+        elements[index] = element;
     }
 
-    private int compare(E e1, E e2) {
-        return comparator != null ? comparator.compare(e1, e2)
-                : ((Comparable<E>)e1).compareTo(e2);
-    }
+
 
     private void emptyCheck() {
         if (size == 0) {
