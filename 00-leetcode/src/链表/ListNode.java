846ec494813c65dc4ab4bb8ad5ac61a7d8f4d0eb
huwd <hwd9053@qq.com>
2021-03-25 22:45:40 +0800

    20210325 night leetcode

--- 00-leetcode/src/链表/ListNode.java
+++ 00-leetcode/src/链表/ListNode.java
@@ -3,7 +3,9 @@ package 链表;
 public class ListNode {
 	int val;
 	ListNode next;
-	public ListNode(int val) {
+	ListNode() {}
+	ListNode(int val) {
 		this.val = val;
 	}
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
